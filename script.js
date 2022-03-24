cd00c2d6320340c0416c698ada4ee991884e7cc5
Julianne Keijzer <91735630+JulianneKeijzer@users.noreply.github.com>
2022-03-24 14:24:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// part 1
+const calculateVAT = function(basePrice, VATPercentage) {
+    return basePrice * (VATPercentage / 100);
+};
+
+const calculatePriceIncludingVAT = function(basePrice, VATPercentage) {
+    const VAT = calculateVAT(basePrice, VATPercentage);
+    return basePrice + VAT;
+};
+
+console.log(calculatePriceIncludingVAT(1000, 21));
+console.log(calculatePriceIncludingVAT(2, 9));
+
+// part 2
+const calculateBasePrice = function(priceIncludingVAT, VATPercentage) {
+    const basePrice = priceIncludingVAT / ((100 + VATPercentage) / 100);
+    return basePrice;
+};
+
+const calculateBasePriceAndVAT = function(priceIncludingVAT, VATPercentage) {
+    const basePrice = calculateBasePrice(priceIncludingVAT, VATPercentage);
+    const VAT = priceIncludingVAT - basePrice;
+    return [basePrice, VAT];
+};
+
+console.log(calculateBasePriceAndVAT(1210, 21));
+console.log(calculateBasePriceAndVAT(2.18, 9));
